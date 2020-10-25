3cb72520144229fb4fcb85e5443aa5de742e08a3
Halil Koşar <kosarhalil@gmail.com>
2020-10-26 00:49:29 +0300

    1.run
    1.run

--- aspnetcoreapi/Controllers/WeatherForecastController.cs
+++ aspnetcoreapi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace aspnetcoreapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mildy", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
